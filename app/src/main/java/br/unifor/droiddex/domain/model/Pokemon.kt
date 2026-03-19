d7b1abea2f12fa9a97767be981fc4292e2c32750
Bruno Lopes <brunolopesjn@gmail.com>
2026-03-18 22:50:40 -0300

    :sparkles: feat: created some domain artifacts

--- app/src/main/java/br/unifor/droiddex/domain/model/Pokemon.kt
+++ app/src/main/java/br/unifor/droiddex/domain/model/Pokemon.kt
@@ -1,3 +1,14 @@
 package br.unifor.droiddex.domain.model
 
-data class Pokemon()
+data class Pokemon(
+    val id: Int,
+    val name: String,
+    val imageUrl: String,
+    val types: List<String>
+) {
+    val displayName:String
+        get() = name.replaceFirstChar { it.uppercase() }
+
+    val formattedId:String
+        get() = "#${id.toString().padStart(3, '0')}"
+}
--- app/src/main/java/br/unifor/droiddex/domain/repository/PokemonRepository.kt
+++ app/src/main/java/br/unifor/droiddex/domain/repository/PokemonRepository.kt
@@ -1,4 +1,9 @@
 package br.unifor.droiddex.domain.repository
 
+import br.unifor.droiddex.domain.model.Pokemon
+
 interface PokemonRepository {
+
+    suspend fun getPokemonList(limit: Int, offset: Int): Result<List<Pokemon>>
+
 }
